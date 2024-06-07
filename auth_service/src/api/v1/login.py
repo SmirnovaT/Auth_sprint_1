1bacaf9c449323452f3eb4bd17bbaad5d3ebb4d9
SmirnovaT <tanya2.93@mail.ru>
2024-06-07 22:31:42 +0300

    Поправила логин

--- auth_service/src/api/v1/login.py
+++ auth_service/src/api/v1/login.py
@@ -1,19 +1,12 @@
-import logging
 from http import HTTPStatus
 
-from fastapi import APIRouter, Depends, Response, HTTPException
-from pydantic import BaseModel
+from fastapi import APIRouter, Depends, Response
 
-from src.db.cache import AsyncCacheService
+
+from src.schemas.user import Login
 from src.services.user import UserService
-from src.utils.jwt import create_access_and_refresh_tokens
 
 router = APIRouter(tags=["login"])
-
-
-class Login(BaseModel):
-    user_login: str
-    password: str
 
 
 @router.post(
@@ -24,26 +17,7 @@ class Login(BaseModel):
 )
 async def login(
     data: Login,
-    responce: Response,
+    response: Response,
     service: UserService = Depends(UserService),
-    cache: AsyncCacheService = Depends(AsyncCacheService),
 ):
-    print(data)
-    user_login = data.user_login
-    password = data.password
-    user = await service.get_user(user_login)
-
-    if user and user.check_password(password):
-        access_token, refresh_token = create_access_and_refresh_tokens(
-            user_login, user.role
-        )
-        logging.info(f"Successfully login")
-        responce.set_cookie("access_token", access_token)
-        responce.set_cookie("refresh_token", refresh_token)
-        cache_key = user_login
-        await cache.create_or_update_record(cache_key, refresh_token)
-        return responce
-    else:
-        raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail="Неверный логин или пароль"
-        )
+    return await service.login(response, data)
